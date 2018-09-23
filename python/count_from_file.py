07e5594f63cff76234ac0cf4d8ddc777a84e3d6a
Ilyas Keskin <ilyas.keskin@posteo.de>
2018-09-23 14:50:09 +0200

    reorganized external data input and adjusted code accordingly

--- python/count_from_file.py
+++ python/count_from_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 name_count = {}
-with open("/home/ilkeskin/code/nameslist.txt") as names:
+with open("data/nameslist.txt") as names:
     for name in names:
         if name.strip() in name_count:
             name_count[name.strip()] += 1
@@ -11,7 +11,7 @@ with open("/home/ilkeskin/code/nameslist.txt") as names:
 print name_count
 
 cat_count = {}
-with open("/home/ilkeskin/code/Training_01.txt") as cats:
+with open("data/Training_01.txt") as cats:
     for cat in cats:
         if cat.split('/')[2] in cat_count:
             cat_count[cat.split('/')[2]] += 1
--- python/happy_primes.py
+++ python/happy_primes.py
@@ -2,7 +2,8 @@
 
 happy_primes = []
 
-with open("primenumbers.txt", 'r') as primes, open("happynumbers.txt", 'r') as happies:
+with    open("data/primenumbers.txt", 'r') as primes,\
+        open("data/happynumbers.txt", 'r') as happies:
     primes = [int(x) for x in primes]
     happies = [int(x) for x in happies]
     happy_primes = [x for x in primes if x in happies]
